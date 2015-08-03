9a721b3db9ded958966d605931f616f361d9fa17
Sergey Lekomtsev <sergey.lekomtsev@paxport.se>
2015-08-03 12:27:01 +0200

    Set headers

--- server/app.js
+++ server/app.js
@@ -3,22 +3,19 @@ var fs = require('fs');
 var app = express();
 var apiVersion = '0.1';
 
-var options = {
-  setHeaders: function (res, path, stat) {
-    res.set('Access-Control-Allow-Origin', '*');
-  }
-};
-
-app.use(express.static('public', options));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
 
 app.get('/api/flight/:flightNo/:date/configuration', function (req, res) {
-  raw = fs.readFileSync('config-response' + apiVersion + '.json', { encoding: 'UTF8' });
-  res.send(raw);
+    raw = fs.readFileSync('config-response' + apiVersion + '.json', {encoding: 'UTF8'});
+    res.send(raw);
 });
 
 var server = app.listen(3000, function () {
-  var host = 'localhost';
-  var port = server.address().port;
+    var host = 'localhost';
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
