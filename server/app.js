d4616298f6fb909fce4ddb5730cd02613b8c38d9
Alexey Zhelonkin <Alexey.Zhelonkin@paxport.se>
2015-08-06 11:19:39 +0200

    request allocation from server

--- server/app.js
+++ server/app.js
@@ -3,19 +3,13 @@ var fs = require('fs');
 var app = express();
 var apiVersion = '0.1';
 
-var flightsMap = {
-    "NVR123": "aircraft_configuration_one_deck.json",
-    "NVR124": "aircraft_configuration_two_deck.json"
-};
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-app.get('/api/flight/:flightNo/:date/configuration', function (req, res) {
-    var fileName = flightsMap[req.params.flightNo];
-    var raw = fs.readFileSync(fileName ? fileName : "aircraft_configuration_one_deck", {encoding: 'UTF8'});
+app.get('/api/flight/:flightNo/:date/allocation', function (req, res) {
+    var raw = fs.readFileSync("allocation.json", {encoding: 'UTF8'});
     res.send(raw);
 });
 
