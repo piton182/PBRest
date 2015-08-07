ff008eb2a787e2bb751eaa9800f8dd6ef0e6cde1
Sergey Lekomtsev <sergey.lekomtsev@paxport.se>
2015-08-07 08:28:50 +0200

    Changes to run all servers using only one file (cherry picked from commit 75d1914)

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var apiVersion = '0.1';
+module.exports.start = function () {
+    var express = require('express');
+    var fs = require('fs');
+    var app = express();
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -9,13 +9,16 @@ app.use(function (req, res, next) {
 });
 
 app.get('/api/flight/:flightNo/:date/allocation', function (req, res) {
-    var raw = fs.readFileSync("allocation.json", {encoding: 'UTF8'});
+    var raw = fs.readFileSync(__dirname + "allocation.json", {encoding: 'UTF8'});
     res.send(raw);
 });
 
-var server = app.listen(3000, function () {
-    var host = 'localhost';
-    var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+    var server = app.listen(3000, function () {
+        var host = 'localhost';
+        var port = server.address().port;
+
+        console.log('Server listening at http://%s:%s', host, port);
+    });
+};
+
