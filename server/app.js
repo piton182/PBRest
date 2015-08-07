6f45beff8574ddb92a9e92291656f15bef93acfe
Sergey Lekomtsev <sergey.lekomtsev@paxport.se>
2015-08-07 08:28:50 +0200

    Changes to run all servers using only one file (cherry picked from commit 75d1914)

--- server/app.js
+++ server/app.js
@@ -1,27 +1,30 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var apiVersion = '0.1';
+module.exports.start = function () {
+    var express = require('express');
+    var fs = require('fs');
+    var app = express();
 
-var flightsMap = {
-    "NVR123": "aircraft_configuration_one_deck.json",
-    "NVR124": "aircraft_configuration_two_deck.json"
+    var flightsMap = {
+        "NVR123": "aircraft_configuration_one_deck.json",
+        "NVR124": "aircraft_configuration_two_deck.json"
+    };
+
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        next();
+    });
+
+    app.get('/api/flight/:flightNo/:date/configuration', function (req, res) {
+        var fileName = flightsMap[req.params.flightNo];
+        var raw = fs.readFileSync(__dirname + "/" + (fileName ? fileName : "aircraft_configuration_one_deck"), {encoding: 'UTF8'});
+        res.send(raw);
+    });
+
+
+    var server = app.listen(3000, function () {
+        var host = 'localhost';
+        var port = server.address().port;
+
+        console.log('Server listening at http://%s:%s', host, port);
+    });
 };
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
-
-app.get('/api/flight/:flightNo/:date/configuration', function (req, res) {
-    var fileName = flightsMap[req.params.flightNo];
-    var raw = fs.readFileSync(fileName ? fileName : "aircraft_configuration_one_deck", {encoding: 'UTF8'});
-    res.send(raw);
-});
-
-var server = app.listen(3000, function () {
-    var host = 'localhost';
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
