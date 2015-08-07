6517c076d890226aced709570f7e80d9fa27230f
Sergey Lekomtsev <sergey.lekomtsev@paxport.se>
2015-08-07 08:28:50 +0200

    Changes to run all servers using only one file (cherry picked from commit 75d1914)

---
+++ dev.js
@@ -0,0 +1,19 @@
+var staticModule = require('./httpserver/app.js');
+var restServer = require('./server/app.js');
+
+var express = require('express');
+var serveStatic = require('serve-static');
+var app = express();
+
+staticModule.start();
+restServer.start();
+
+app.use(serveStatic(__dirname + "/client"));
+
+var server = app.listen(3002, function () {
+    var host = 'localhost';
+    var port = server.address().port;
+
+    console.log('Client server listening at http://%s:%s', host, port);
+});
+
